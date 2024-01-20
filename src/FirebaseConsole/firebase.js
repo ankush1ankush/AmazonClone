ad00b56d8b539a2e7b3a369a2bc73a6a14749245
ankush1ankush <2040119@sliet.ac.in>
2024-01-21 01:47:00 +0530

    remove key

--- src/FirebaseConsole/firebase.js
+++ src/FirebaseConsole/firebase.js
@@ -9,17 +9,7 @@ require("dotenv").config();
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-/*
-const firebaseConfig = {
-  apiKey: "AIzaSyAZmYazovaaf1DZbbtM3GYwHdIzKHMM6CA",
-  authDomain: "clone-295a6.firebaseapp.com",
-  projectId: "clone-295a6",
-  storageBucket: "clone-295a6.appspot.com",
-  messagingSenderId: "278278145914",
-  appId: "1:278278145914:web:897c85a87cb450f07338c3",
-  measurementId: "G-V5SX8PNWWM",
-};
-*/
+
 const firebaseConfig = {
   apiKey: process.env.APIKEY,
   authDomain:  process.env.AUTHDOMAIN,
